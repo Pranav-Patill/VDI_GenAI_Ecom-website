0986c852c4cfa613e8fa1f87177bc52fee6e9869
Pranav.Patil <pranav.patil@smith-nephew.com>
2023-10-26 21:02:55 +0530

    modified models, view, cart, home

--- website/models.py
+++ website/models.py
@@ -4,21 +4,21 @@ from sqlalchemy.sql import func
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    product_id= db.Column(db.String(5))
     name = db.Column(db.String(150))
     price = db.Column(db.Float)
     image = db.Column(db.String(150))
     description = db.Column(db.String(150))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # cart_list = db.relationship('Cart')
+    cart_item = db.relationship('Cart')
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cart_item = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cart_item = db.Column(db.String, db.ForeignKey('product.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #foreign key stored in child 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    cart_list = db.relationship('Cart')
+    cart_item = db.relationship('Cart') #parent
--- website/views.py
+++ website/views.py
@@ -6,15 +6,7 @@ import json
 
 views = Blueprint('views', __name__)
 
-products = [
-  {'name': 'Product 1', 'image': 'product1.jpg', 'description': 'Description 1','product_id':'p1','price':100},
-  {'name': 'Product 2', 'image': 'product2.jpg', 'description': 'Description 2','product_id':'p2','price':150},
-  {'name': 'Product 3', 'image': 'product3.jpg', 'description': 'Description 3','product_id':'p3','price':45},
-  {'name': 'Product 4', 'image': 'product4.jpg', 'description': 'Description 4','product_id':'p4','price':600},
-  {'name': 'Product 5', 'image': 'product5.jpg', 'description': 'Description 5','product_id':'p5','price':400},
-  {'name': 'Product 6', 'image': 'product6.jpg', 'description': 'Description 6','product_id':'p6','price':220},
-  {'name': 'Product 7', 'image': 'product7.jpg', 'description': 'Description 7','product_id':'p7','price':10}
-  ]
+
 
 @views.route('/',methods=['GET', 'POST'])
 def index():
@@ -26,13 +18,29 @@ def index():
 @views.route('/home',methods=['GET', 'POST']) 
 @login_required
 def home():
-
+  products = [
+  {'name': 'Product 1', 'image': 'product1.jpg', 'description': 'Description 1','product_id':'p1','price':100},
+  {'name': 'Product 2', 'image': 'product2.jpg', 'description': 'Description 2','product_id':'p2','price':150},
+  {'name': 'Product 3', 'image': 'product3.jpg', 'description': 'Description 3','product_id':'p3','price':45},
+  {'name': 'Product 4', 'image': 'product4.jpg', 'description': 'Description 4','product_id':'p4','price':600},
+  {'name': 'Product 5', 'image': 'product5.jpg', 'description': 'Description 5','product_id':'p5','price':400},
+  {'name': 'Product 6', 'image': 'product6.jpg', 'description': 'Description 6','product_id':'p6','price':220},
+  {'name': 'Product 7', 'image': 'product7.jpg', 'description': 'Description 7','product_id':'p7','price':10}
+  ]
   return render_template("home.html", user=current_user, items=products)
 
 @views.route('/cart', methods=['GET', 'POST'])
 @login_required
 def cart():
-    return render_template("cart.html", user=current_user, cart_items=products)
+  cart_items = [
+  {'name': 'Product 1', 'image': 'product1.jpg', 'description': 'Description 1','product_id':'p1','price':100,'quantity':4},
+  {'name': 'Product 2', 'image': 'product2.jpg', 'description': 'Description 2','product_id':'p2','price':150,'quantity':2},
+  {'name': 'Product 3', 'image': 'product3.jpg', 'description': 'Description 3','product_id':'p3','price':45,'quantity':7}
+  ]
+  
+  total = sum(item['price'] * item['quantity'] for item in cart_items)
+  
+  return render_template("cart.html", user=current_user, cart_items=cart_items, cart_total=total)
 
 @views.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
